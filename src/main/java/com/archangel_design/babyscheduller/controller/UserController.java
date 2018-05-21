31c55f1c69ccf0d8f1a1101898ef94b1e379755f
Raff <archangel.raffael@gmail.com>
2018-05-21 19:17:55 +0200

    Imports organized

--- src/main/java/com/archangel_design/babyscheduller/controller/UserController.java
+++ src/main/java/com/archangel_design/babyscheduller/controller/UserController.java
@@ -13,7 +13,12 @@ import com.archangel_design.babyscheduller.service.LocationService;
 import com.archangel_design.babyscheduller.service.SessionService;
 import com.archangel_design.babyscheduller.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * Main controller responsible for user operations
