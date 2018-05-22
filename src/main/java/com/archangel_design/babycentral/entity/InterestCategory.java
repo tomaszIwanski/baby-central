e632246839c4a87c6125275d524eccf6f21cfae1
Raff <archangel.raffael@gmail.com>
2018-05-22 08:09:51 +0200

    Interest entity

---
+++ src/main/java/com/archangel_design/babycentral/entity/InterestCategory.java
@@ -0,0 +1,18 @@
+/*
+ * Baby Central
+ * Copyright (c) 2018.
+ * Rafal Martinez-Marjanski
+ */
+
+package com.archangel_design.babycentral.entity;
+
+/**
+ * Interest category
+ */
+public enum InterestCategory {
+    ART,
+    MUSIC,
+    SPORT,
+    FOOD,
+    TRAVEL;
+}
--- src/main/java/com/archangel_design/babycentral/entity/InterestEntity.java
+++ src/main/java/com/archangel_design/babycentral/entity/InterestEntity.java
@@ -6,15 +6,52 @@
 
 package com.archangel_design.babycentral.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
+/**
+ * Represents user's single interest.
+ */
 @Entity
 @Table(name = "interests")
 public class InterestEntity {
+    /**
+     * Unique id.
+     */
     @Id
     @GeneratedValue
     private Long id;
+
+    /**
+     * Keyword.
+     */
+    @Column(length = 50)
+    private String keyword;
+
+    /**
+     * Interest category
+     */
+    @Enumerated(value = EnumType.STRING)
+    private InterestCategory category;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public InterestEntity setKeyword(String keyword) {
+        this.keyword = keyword;
+        return this;
+    }
+
+    public InterestCategory getCategory() {
+        return category;
+    }
+
+    public InterestEntity setCategory(InterestCategory category) {
+        this.category = category;
+        return this;
+    }
 }
