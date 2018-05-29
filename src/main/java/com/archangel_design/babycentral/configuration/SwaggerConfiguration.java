d8c1b8b272d4b42d2f027c2c0e39dd1abea802de
Raff <archangel.raffael@gmail.com>
2018-05-29 17:04:45 +0200

    default error handler removed from documentation

--- src/main/java/com/archangel_design/babycentral/configuration/SwaggerConfiguration.java
+++ src/main/java/com/archangel_design/babycentral/configuration/SwaggerConfiguration.java
@@ -1,5 +1,6 @@
 package com.archangel_design.babycentral.configuration;
 
+import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -16,7 +17,7 @@ public class SwaggerConfiguration {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
                 .paths(PathSelectors.any())
                 .build();
     }
